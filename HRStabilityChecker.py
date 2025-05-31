cc3cd29db8d5f5362daa99d3dbba1f58940ec170
mpsloan <sloan.40@wright.edu>
2025-05-31 16:05:19 -0400

    add comments

--- HRStabilityChecker.py
+++ HRStabilityChecker.py
@@ -1,5 +1,5 @@
 
-
+# input and output files opened
 in_file = open('input/HRInput.txt', 'r')
 out_file = open('output/HROutput.txt', 'w')
 
@@ -8,7 +8,12 @@ in_lines = in_file.readlines()
 num_hospitals = int(in_lines[0])
 num_residents = int(in_lines[1])
 
+# hospitals is a dictionary
+# the key is the hospital's name
+# the values are their preferred list of residents (in decreasing order)
 hospitals = {}
+
+# offset is 2 because the first two lines of the file have already been read
 offset = 2
 i = 0
 
@@ -19,7 +24,12 @@ while i < num_hospitals:
     i += 1
 
 
+# residents is a dictionary
+# the key is the resident's name
+# the values are their preferred list of hospitals (in decreasing order)
 residents = {}
+
+# offset is increased because the hospitals have now been read
 offset = offset + num_hospitals
 j = 0
 
@@ -28,5 +38,9 @@ while j < num_residents:
     residents[parts[0]] = parts[1:]
     j += 1
 
+
+
+in_file.close()
+out_file.close()
 
 
