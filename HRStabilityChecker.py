c6bb1d793a21541828944a7d58e2d93d5346401d
mpsloan <sloan.40@wright.edu>
2025-06-07 15:09:51 -0400

    can cycle through assignment list as wanted

--- HRStabilityChecker.py
+++ HRStabilityChecker.py
@@ -2,6 +2,14 @@
 
 # def is_unstable(resident, hospital, matches)
 
+def get_resident_list(resident_name):
+    return residents_pref[resident]
+
+
+def get_hospital_list(hospital_name):
+    return hospitals_pref[hospital_name]
+
+
 # input and output files opened
 in_file = open('input/HRInput.txt', 'r')
 out_file = open('output/HROutput.txt', 'w')
@@ -32,7 +40,7 @@ while i < num_hospitals:
 # residents is a dictionary
 # the key is the resident's name
 # the values are their preferred list of hospitals_pref (in decreasing order)
-residents = {}
+residents_pref = {}
 
 # offset is increased because the hospitals_pref have now been read
 offset = offset + num_hospitals
@@ -40,7 +48,7 @@ j = 0
 
 while j < num_residents:
     parts = in_lines[j + offset].split()
-    residents[parts[0]] = parts[1:]
+    residents_pref[parts[0]] = parts[1:]
     j += 1
 
 match_file = open('input/HRMatch.txt', 'r')
@@ -52,15 +60,13 @@ for lines in match_lines:
     parts = lines.split()
     assignments[parts[0]] = parts[2:]
 
-# cycle through assignments
-# check if resident prefers other hospital to current
-# if yes unstable matching, break and write NO to file
-# if no
-# for assignment in assignments:
 
+for assigned_hospital in assignments:
+    print(assigned_hospital)
+    matched_residents = assignments[assigned_hospital]
+    for matched_resident in matched_residents:
+        print(matched_resident)
 
-print(assignments)
-print(residents.get('DARRIUS')[0])
 
 in_file.close()
 out_file.close()
